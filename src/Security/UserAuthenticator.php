350d6bc512fb20c8c5de9a19f005f609bf6b4c67
Ronan Martin <martin.ronan@gmail.com>
2021-06-03 10:10:07 +0200

    Add route for AuthenticationSuccess and verified email address

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -94,9 +94,10 @@ class UserAuthenticator extends AbstractFormLoginAuthenticator implements Passwo
         if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
             return new RedirectResponse($targetPath);
         }
-
+        
+        return new RedirectResponse($this->urlGenerator->generate('hello'));
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl()
