64e098acf17f08dc93b98f1ffa95241695e3e22a
Tony Bengué <tonybengue@hotmail.fr>
2021-06-24 10:37:34 +0200

    Improve erase and CSS

--- src/Controller/DrawingController.php
+++ src/Controller/DrawingController.php
@@ -14,6 +14,7 @@ class DrawingController extends AbstractController
     public function index(): Response
     {
         return $this->render('drawing/index.html.twig', [
+        // return $this->render('drawing/index.html', [
             'controller_name' => 'DrawingController',
         ]);
     }
