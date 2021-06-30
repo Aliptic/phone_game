66e7b6faee5195f59b0a661cea8c6b6e4b28812c
Ronan Martin <martin.ronan@gmail.com>
2021-06-30 11:15:30 +0200

    Add Text phase

--- src/Controller/TextController.php
+++ src/Controller/TextController.php
@@ -99,4 +99,103 @@ class TextController extends AbstractController
             'waiting' => '0',
         ]);
     }
+
+    /**
+     * @Route("/text/{id}", name="text")
+     */
+    public function text(Request $request, int $id, HubInterface $hub ): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        // we try to find which player is before our player
+        $game=$this->getDoctrine()
+        ->getRepository(Game::class)
+        ->findOneBy(array('id' => $id));
+     
+        $playersList=$game->getUsersId();
+
+        // player's position in the ranking of players in this game
+        $myPosition=array_search($this->getUser()->getId(), array_column($playersList, '0'));
+
+        // opponent's position is ours -1 unless we are the first on the list
+        if($myPosition == 0) {
+            $opponentPosition = count($playersList)-1;
+        } else {
+            $opponentPosition = $myPosition-1;
+        }
+        
+        // id of our "opponent" that we will look for in History
+        $opponentId=$playersList[$opponentPosition][0];
+        $historyOpponent=$this->getDoctrine()
+            ->getRepository(History::class)
+            ->findOneBy(array('game_id' => $id,'user_id' => $opponentId));
+        
+        // find the correct drawing to display
+        $size=count($historyOpponent->getHistory());
+        // takes size-1 to fall back on the correct drawing in case it is a second phase of text
+        $drawOpponent=$historyOpponent->getHistory()[$size-1];
+        
+        dump($drawOpponent);
+        
+        $formText = $this->createFormBuilder()
+            ->add('phrase', TextType::class, ['label' => 'phrase'])
+            ->add('Validate', SubmitType::class, ['label' => 'Validate'])
+            ->setMethod('POST')
+            ->getForm();
+        
+        $formText->handleRequest($request);
+
+        if ($formText->isSubmitted()) {
+            $phrase = $formText->get('phrase')->getData();
+            dump($phrase);
+
+            $history=$this->getDoctrine()
+            ->getRepository(History::class)
+            ->findOneBy(['game_id' => $id,'user_id' => $this->getUser()->getId()]);
+
+            $history->setHistory([$phrase]);
+
+            $entityManager->flush();
+            
+            //Check that everyone has filled in their history
+            $query = "SELECT history FROM history h WHERE game_id = ".$id;
+            $statement = $connection->prepare($query);
+            $statement->execute();
+            $histories = $statement->fetchAll();  
+
+            $vide = 0;
+            foreach($histories as $tab) {
+                if($tab['history'] == "[]"){
+                    $vide++;
+                }
+            }
+
+            // if all players have validated this step
+            if($vide == 0) {
+                // new sse update to send to drawing
+                $url = 'http://localhost:8080/text/'.$id;
+                $update = new Update(
+                    $url,
+                    json_encode(array('subject' => 'draw',))
+                );
+                $hub->publish($update);
+                
+                //in case of bug, it forces the page to redirect to the next step of the game
+                return $this->redirectToRoute('drawing',[
+                    "id" => $id,
+                ]);
+            } else {    // if a player has not still validated, it warns the other players
+                return $this->render('text/text.html.twig', [
+                    'formText' => $formText->createView(),
+                    'game_id' => $id,
+                    'waiting' => '1',
+                ]);
+            }
+            
+        }
+        return $this->render('text/text.html.twig', [
+                'drawing' => $drawOpponent,
+                'formText' => $formText->createView(),
+        ]);
+    }
 }
