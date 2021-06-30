b526dc18c5d2bda9231df546e16e711382eb2d71
QuentinJoly <quentin.joly3@gmail.com>
2021-06-30 16:51:01 +0200

    new way of finding the right history

--- src/Controller/DrawingController.php
+++ src/Controller/DrawingController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
 class DrawingController extends AbstractController
 {
@@ -18,23 +19,35 @@ class DrawingController extends AbstractController
      */
     public function index(Request $request, int $id): Response
     {   
+        $entityManager = $this->getDoctrine()->getManager();
+
         // on cherche à retrouver quel joueur est "l'adversaire" de notre joueur
         $game=$this->getDoctrine()
             ->getRepository(Game::class)
             ->findOneBy(array('id' => $id));
         
         $playersList=$game->getUsersId();
+        dump($playersList);
 
-        //position du joueur dans le classment des joueurs de cette partie
+        //position du joueur dans le classement des joueurs de cette partie
         $myPosition=array_search($this->getUser()->getId(), array_column($playersList, '0'));
 
-        //la position de l'adversaire est la notre -1 sauf si on est le premier de la liste
-        if($myPosition == 0){
-            $opponentPosition = count($playersList)-1;
-        }else{
-            $opponentPosition = $myPosition-1;
-        }
+        $myHistory=$this->getDoctrine()
+            ->getRepository(History::class)
+            ->findOneBy(array('game_id' => $id,'user_id' => $this->getUser()->getId()));
+        // manche actuelle
+        $round=count($myHistory->getHistory())+1;
         
+        // 
+        if(($myPosition - $round)+1<0){
+            $offset=$round-($myPosition+1);
+            dump("j'ai besoin d'un offset de: ".$offset);
+            $opponentPosition=count($playersList)-$offset;
+        }else{
+            $opponentPosition = ($myPosition - $round)+1;
+        }
+        dump("mypos ".$myPosition." round".$round);
+        dump("oppos".$opponentPosition);
         //l'id de notre adversaire que l'on va chercher dans les History
         $opponentId=$playersList[$opponentPosition][0];
         $historyOpponent=$this->getDoctrine()
@@ -45,17 +58,28 @@ class DrawingController extends AbstractController
         $size=count($historyOpponent->getHistory());
         //on prend size-1 pour retomber sur la bonne phrase au cas où il s'agit d'une deuxieme phase de dessin
         $sentenceOpponent=$historyOpponent->getHistory()[$size-1];
-        
-        dump($sentenceOpponent);
 
         $formDraw = $this->createFormBuilder()
-            ->add('Validate', SubmitType::class, ['label' => 'Validate'])
+            ->add('validate', SubmitType::class, ['label' => 'Validate'])
+            ->add('hidden', HiddenType::class)
             ->setMethod('POST')
             ->getForm();
         $formDraw->handleRequest($request);
 
         if ($formDraw->isSubmitted()) {
+            // récuperation du dessin caché dans le form
+            $drawing = $formDraw->get('hidden')->getData();
+
+            $history=$this->getDoctrine()
+                ->getRepository(History::class)
+                ->findOneBy(['game_id' => $id,'user_id' => $opponentId]);
             
+            // une façon moins moche existe surement, pas le temps déso pas déso
+            $newhistory=$history->getHistory();
+            array_push($newhistory,$drawing);
+            $history->setHistory($newhistory);
+
+            $entityManager->flush();
         }
 
         return $this->render('drawing/index.html.twig', [
