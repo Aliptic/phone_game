e3781c5aff96b28bcb5281b18c64250d8b00f8a9
Ronan Martin <martin.ronan@gmail.com>
2021-07-02 22:51:39 +0200

    Home page modified

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Game;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
@@ -14,23 +15,34 @@ class HomeController extends AbstractController
      */
     public function index(TranslatorInterface $translator): Response
     {
+        $entityManager = $this->getDoctrine()->getManager();   
+        
+        $invitation = 0;
+        $numGame = 0;
+        
         if($this->get('session')->get('token'))
-        {
-            return $this->redirectToRoute('player_invite');
+        {   
+            $token = $this->get('session')->get('token');
+            // database query with token filter
+            $game = $this->getDoctrine()
+                ->getRepository(Game::class)
+                ->findOneBy(['room_token' => $token]);
+              
+            // verify if a token exist in base, if not, the player is warned
+            if (!$game) {
+                // delete the session variable token because it's useless now
+                $this->get('session')->set('token', NULL);
+            }    
+            $numGame = $game->getId();
+            $invitation = 1;
+        //    return $this->redirectToRoute('player_invite');
         }
-    /*    elseif($this->getUser()->getId())
-        {
-            $player = $this->getUser()->getId();
-
-        }
-    */
-        else 
-        {
+        
         //    $message = $translator->trans('HomeController');
-            
-            return $this->render('index/index.html.twig', [
+        return $this->render('index/index.html.twig', [
         //    'controller_name' => $message,
-            ]);
-        }
+            'invitation' => $invitation,
+            'numGame' => $numGame,
+        ]);
     }
 }
